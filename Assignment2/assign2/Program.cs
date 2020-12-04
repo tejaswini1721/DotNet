9c4fc23ca5c6ad3a64a3248f73c957975237afd8
tejaswini1721 <65243728+tejaswini1721@users.noreply.github.com>
2020-12-04 08:30:58 +0530

    Add files via upload

---
+++ Assignment2/assign2/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager mg = new Manager("teju", 1, 68000, "developer");
+            Console.WriteLine(mg.EmpName + " " + mg.calcNetSalary());
+
+            GeneralManager gm = new GeneralManager("teju", 2, 75000, "trainee", "fs");
+            Console.WriteLine(gm.EmpName + " " + gm.calcNetSalary());
+
+            CEO ce = new CEO("teju", 10, 850000);
+            Console.WriteLine(ce.EmpName + " " + ce.calcNetSalary());
+            Console.ReadLine();
+        }
+    }
+    public abstract class Employee
+    {
+        private string empName;
+        private int empNo;
+        public static int id;
+        private short deptNo;
+        public decimal basicSal;
+        public abstract decimal calcNetSalary();
+
+        public string EmpName
+        {
+            get { return empName; }
+            set { if (value == null) { Console.WriteLine("name can't be null"); } else { empName = value; } }
+        }
+        public int EmpNo { get; }
+        public short DeptNo
+        {
+            get { return deptNo; }
+            set
+            {
+                if (value > 0)
+                {
+                    deptNo = value;
+                }
+                else
+                {
+                    Console.WriteLine("invalid number");
+                }
+            }
+        }
+        public abstract decimal BasicSal
+        {
+            get; set;
+        }
+
+        public Employee(string empName, short deptNo, decimal basicSal)
+        {
+            id++;
+            this.EmpNo = id;
+            this.EmpName = empName;
+            this.DeptNo = deptNo;
+            this.BasicSal = basicSal;
+        }
+    }
+    public class Manager : Employee
+    {
+        string designation;
+        public string Designation
+        {
+            get { return designation; }
+            set
+            {
+                if (value == null)
+                {
+                    Console.WriteLine("designation can't be null");
+                }
+                else
+                {
+                    designation = value;
+                }
+
+            }
+        }
+        public override decimal BasicSal { get; set; }
+        public Manager(string empName, short deptNo, decimal basicSal, string designation) : base(empName, deptNo, basicSal)
+        {
+            this.Designation = designation;
+        }
+        public override decimal calcNetSalary()
+        {
+            decimal bonus = 2000;
+            decimal netSal = BasicSal + bonus;
+            return netSal;
+        }
+
+    }
+    public class GeneralManager : Manager
+    {
+        private string perks;
+        public string Perks
+        {
+            get { return perks; }
+            set { perks = value; }
+        }
+        public GeneralManager(string empName, short deptNo, decimal basicSal, string designation, string perks) : base(empName, deptNo, basicSal, designation)
+        {
+            this.Perks = perks;
+        }
+        public override decimal calcNetSalary()
+        {
+            decimal bonus = 2000;
+            decimal netSal = BasicSal + bonus;
+            return netSal;
+        }
+    }
+    public class CEO : Employee
+    {
+        public CEO(string empName, short deptNo, decimal basicSal) : base(empName, deptNo, basicSal)
+        {
+
+        }
+        public override decimal BasicSal { get; set; }
+        public override decimal calcNetSalary()
+        {
+            decimal bonus = 2000;
+            decimal netSal = BasicSal + bonus;
+            return netSal;
+        }
+    }
+
+
+}
