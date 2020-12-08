a00a2623d48b605e482e6177d6d354c9c9a70ee7
tejaswini1721 <65243728+tejaswini1721@users.noreply.github.com>
2020-12-08 11:23:33 +0530

    Add files via upload

---
+++ Assignment4-3/Assign4-3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign4_3
+{
+
+    public class Employee
+    {
+        int empno;
+        string name;
+        string designation;
+        int age;
+
+        public Employee(int empno, string empname, string designation, int age)
+        {
+            this.empno = empno;
+            this.name = empname;
+            this.designation = designation;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return empno + " " + name + " " + designation + " " + age;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee e1 = new Employee(1, "teju", "manager", 25);
+            Employee e2 = new Employee(2, "sonu", "genral manager", 27);
+            List<Employee> list = new List<Employee>();
+
+            list.Add(e1);
+            list.Add(e2);
+
+            //foreach(Employee li in list)
+            // {
+            //     Console.WriteLine(li);
+            // }
+
+            Employee[] emp = list.ToArray();
+
+            foreach (Employee lio in emp)
+            {
+                Console.WriteLine(lio);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment4-3a/Assign4-3a/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign4_3a
+{
+
+    public class Employee
+    {
+        int empno;
+        string empname;
+        string designation;
+        int age;
+
+        public Employee(int empno, string empname, string designation, int age)
+        {
+            this.empno = empno;
+            this.empname = empname;
+            this.designation = designation;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return empno + " " + empname + " " + designation + " " + age;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the how many employee you want : ");
+            int size = int.Parse(Console.ReadLine());
+            Employee[] e1 = new Employee[size];
+
+            for (int i = 0; i < e1.Length; i++)
+            {
+                Console.WriteLine("\nEnter empno : ");
+                int no = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter empname: ");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Enter designation : ");
+                string design = Console.ReadLine();
+
+                Console.WriteLine("Enter age : ");
+                int age = int.Parse(Console.ReadLine());
+
+                Employee e = new Employee(no, name, design, age);
+
+                e1[i] = e;
+
+            }
+
+            // for(int i =0;i<e1.Length;i++)
+            //{
+            //    Console.WriteLine(e1[i]);
+            //}
+
+
+            Console.WriteLine("\nDeatils of employee after converstion");
+
+            List<Employee> list = new List<Employee>(e1);
+
+            foreach (Employee li in list)
+            {
+                Console.WriteLine(li);
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
