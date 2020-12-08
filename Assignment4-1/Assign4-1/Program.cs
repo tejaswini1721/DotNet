fce332326655bbe3ce690678ff43ae0e52e34fae
tejaswini1721 <65243728+tejaswini1721@users.noreply.github.com>
2020-12-08 11:09:01 +0530

    Add files via upload

---
+++ Assignment4-1/Assign4-1/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign4_1
+{
+
+    public class Employee
+    {
+        public int empno;
+
+        public string empname;
+
+        public string designation;
+
+        public decimal salary;
+
+        public void setvalues(int empno, string empname, string designation, decimal salary)
+        {
+            this.empno = empno;
+            this.empname = empname;
+            this.designation = designation;
+            this.salary = salary;
+        }
+
+        public void show()
+        {
+            Console.WriteLine(empno);
+            Console.WriteLine(empname);
+            Console.WriteLine(designation);
+            Console.WriteLine(salary);
+
+
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("How many employee you want to enter = ");
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            Employee[] e1 = new Employee[capacity];
+
+            for (int i = 0; i < e1.Length; i++)
+            {
+
+                Console.WriteLine("\nEnter empno = ");
+
+                int empno = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter Name = ");
+
+                string empname = Console.ReadLine();
+
+                Console.WriteLine("Enter Designation = ");
+
+                string designation = Console.ReadLine();
+
+                Console.WriteLine("Enter salary = ");
+
+                decimal salaray = decimal.Parse(Console.ReadLine());
+
+                Employee temp = new Employee();
+
+                temp.setvalues(empno, empname, designation, salaray);
+
+                e1[i] = temp;
+
+
+            }
+
+
+            Console.WriteLine("\n*****List of Employees with deatils*****\n");
+
+            for (int i = 0; i < e1.Length; i++)
+            {
+                e1[i].show();
+
+            }
+
+            decimal max = e1[0].salary;
+
+
+            Console.WriteLine("\n*****List of Employees with Highest salaray*****\n");
+
+            for (int i = 0; i < e1.Length; i++)
+            {
+
+                if (e1[i].salary > max)
+                {
+                    max = e1[i].salary;
+
+                    e1[i].show();
+                }
+
+            }
+
+
+            //Console.WriteLine("\nHighest salary of employee is = "+ max + "\n");
+
+
+            Console.WriteLine("\n Enter the element to be serched : ");
+
+            int search = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < e1.Length; i++)
+            {
+
+                if (e1[i].empno == search)
+                {
+                    e1[i].show();
+                }
+
+            }
+
+
+
+            Console.ReadLine();
+
+
+        }
+
+    }
+}
---
+++ Assignment4-2/Assign4-2/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign4_2
+{
+
+    public struct Student
+    {
+
+        private string name;
+
+        public string StudName
+        {
+            set
+            {
+                if (value != null)
+                {
+                    name = value;
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid name");
+                }
+
+            }
+            get
+            {
+                return name;
+            }
+        }
+
+
+        private int rollno;
+
+        public int studrollno
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    rollno = value;
+                }
+            }
+
+            get
+            {
+                return rollno;
+            }
+        }
+
+        private decimal marks;
+
+        public decimal studMarks
+        {
+            set
+            {
+                if (value != 0)
+                {
+                    marks = value;
+                }
+                else
+                {
+                    Console.WriteLine("Marks are Invalid");
+                }
+            }
+            get
+            {
+                return marks;
+            }
+        }
+
+        public Student(string name1, int rollno1, decimal marks1) : this()
+        {
+
+            this.StudName = name1;
+            this.studrollno = rollno1;
+            this.studMarks = marks1;
+
+
+        }
+
+
+        public void show()
+        {
+
+            Console.WriteLine(StudName);
+            Console.WriteLine(studrollno);
+            Console.WriteLine(studMarks);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many student you want : ");
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            Student[] s1 = new Student[capacity];
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                Console.WriteLine("\nEnter Student name : ");
+
+                string name = Console.ReadLine();
+
+                Console.WriteLine("\nEnter Student rollno : ");
+
+                int rollno = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("\nEnter Student Marks : ");
+
+                decimal marks = decimal.Parse(Console.ReadLine());
+
+                Student temp = new Student(name, rollno, marks);
+
+                s1[i] = temp;
+            }
+
+            Console.WriteLine("\n*****Details of Student*****\n");
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                s1[i].show();
+            }
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
